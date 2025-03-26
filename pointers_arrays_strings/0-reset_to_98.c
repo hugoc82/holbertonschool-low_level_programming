ec17ca9cd60544aef3cc672d74e7a597ece3f9d7
hugoc82 <hugocapelle@orange.fr>
2025-03-26 12:19:33 +0000

    .

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Met à jour la valeur de l'entier pointé par n à 98
+ * @n: Le pointeur vers l'entier
+ *
+ * Description: Cette fonction modifie la valeur de l'entier pointé par
+ * le pointeur n pour qu'elle soit égale à 98.
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
